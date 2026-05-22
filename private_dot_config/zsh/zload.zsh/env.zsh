8d5c45570e6bff353724657efbc3d8bd5c9d5a9d
_404 <_404@example.com>
2026-05-22 16:18:19 -0400

    chore

--- private_dot_config/zsh/zload.zsh/env.zsh
+++ private_dot_config/zsh/zload.zsh/env.zsh
@@ -29,6 +29,7 @@ _env_path() {
     "${CARGO_HOME:-${XDG_DATA_HOME:-$HOME/.local/share}/cargo}/bin"
     "${GOBIN:-${XDG_DATA_HOME:-$HOME/.local/share}/go/bin}"
     "${npm_config_prefix:-${XDG_DATA_HOME:-$HOME/.local/share}/npm}/bin"
+    "${CODEX_BIN:-$HOME/_404/.local/share/codex/bin}"
   )
 
   prefix=()
