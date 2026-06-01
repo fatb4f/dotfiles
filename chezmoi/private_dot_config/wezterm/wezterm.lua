f532907b5273a63c212d70750a3a43e5dd27d29b
_404 <_404@example.com>
2026-06-01 03:52:48 -0400

    fix

--- chezmoi/private_dot_config/wezterm/wezterm.lua
+++ chezmoi/private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local shell = require("modules.shell")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -11,6 +12,12 @@ config.hide_tab_bar_if_only_one_tab = false
 config.window_close_confirmation = "NeverPrompt"
 
 config.scrollback_lines = 10000
+config.keys = config.keys or {}
+table.insert(config.keys, {
+  key = "Enter",
+  mods = "SHIFT",
+  action = act.SendString("\r"),
+})
 
 require("modules.smart_splits").apply_to_config(config)
 require("modules.workspaces").apply_to_config(config)
