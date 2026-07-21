448d8f9cc5a48a7e2800c9892fdbd56834bceec4
Bruce Lavoie <f4t.bruce@gmail.com>
2026-07-21 15:28:28 -0400

    Declare committed snapshot properties

---
+++ .codex/context-hydrators/git/internal/hydrator/properties.go
@@ -0,0 +1,12 @@
+package hydrator
+
+var DeclaredPropertyIDs = []string{
+	"determinism",
+	"rename-content-preserved",
+	"content-edit-content-changed",
+	"unrelated-entry-preserved",
+	"mode-change-content-preserved",
+	"symlink-not-traversed",
+	"submodule-not-traversed",
+	"revision-bound",
+}
