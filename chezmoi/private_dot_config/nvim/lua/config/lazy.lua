1d262783df20802112a97b4a84a0d551cd61b6da
_404 <_404@example.com>
2026-07-25 21:59:10 -0400

    cjhore

--- chezmoi/private_dot_config/nvim/lua/config/lazy.lua
+++ chezmoi/private_dot_config/nvim/lua/config/lazy.lua
@@ -22,8 +22,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.snacks_explorer" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.test.core" },
-		{ import = "lazyvim.plugins.extras.lang.go" },
-		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "plugins" },
 	},
 	defaults = {
