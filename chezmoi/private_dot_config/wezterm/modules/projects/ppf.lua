acb3a8c8b1a3657e2234248f7f7aba032d35140e
_404 <_404@example.com>
2026-07-31 11:58:27 -0400

    chore

---
+++ chezmoi/private_dot_config/wezterm/modules/projects/ppf.lua
@@ -0,0 +1,10 @@
+return {
+	id = "ppf",
+	label = "ppf",
+	workspace = "ppf",
+	cwd = "~/src/ppf",
+
+	env = {
+		GIT_KATAS_ROOT = "~/src/ppf",
+	},
+}
---
+++ chezmoi/private_dot_config/wezterm/modules/projects/python.lua
@@ -0,0 +1,10 @@
+return {
+	id = "python",
+	label = "python",
+	workspace = "python",
+	cwd = "~/src/python",
+
+	env = {
+		GIT_KATAS_ROOT = "~/src/python",
+	},
+}
